5a5659557de423e65752bdbe1e264012e6ace815
mjfwilliams <mjf2@williams.edu>
2013-07-16 11:19:23 -0400

    commented hitandrun better

--- R/hitandrun.R
+++ R/hitandrun.R
@@ -27,22 +27,27 @@
 #' b = c(b,0)
 #' w = hitandrun(A, b, n = 100) 
 hitandrun <- function(A, b = NULL, x0 = NULL, n, discard = 0, skiplength = 5, verbose = FALSE) {
-  if(n <= 0 || n %% 1 != 0){
-    stop("n must be a positive integer")
-  }
-  
+    
+    if(n <= 0 || n %% 1 != 0) {
+      stop("n must be a positive integer")
+    }
+    ## should use x0 or b, not both
     stopifnot(!is.null(x0) || !is.null(b))
+    
     if(is.null(x0)) {
         str = "Finding an intial solution..."
         if(verbose) cat(str)
-        ##l = lsei(E = A, F = b, G = diag(ncol(A)), H = rep(0, ncol(A)))$X
+        
+        ## l = (closest point to zero)
         l = t(A) %*% solve(A %*% t(A)) %*% b
+        ## if l isn't in feasible space mirror it
         if(!(all(l > 0))) {
             if(verbose) for(i in 1:nchar(str)) cat("\b")
             str = "Using mirror algorithm to find inner solution...\n"
             if(verbose) cat(str)
+            y = mirror(A, l, 1, verbose)
         }
-        y = mirror(A, l, 1, verbose)
+
         if(verbose) for(i in 1:nchar(str)) cat("\b")
     } else {
         y = x0;
@@ -62,28 +67,29 @@ hitandrun <- function(A, b = NULL, x0 = NULL, n, discard = 0, skiplength = 5, ve
     for(i in 1:(n*skiplength+discard)) {
         tmin=0;tmax=0;
         while(tmin ==0 && tmax ==0) {
-            ## r is a random unit vector
+            ## r is a random unit vector in with basis in Z
             r = rnorm(ncol(Z))
             r = r/sqrt(sum(r^2))
 
-            ## d is a unit vector in the appropriate k-plane pointing in a
+            ## u is a unit vector in the appropriate k-plane pointing in a
             ## random direction
             u = Z%*%r
             c = y/u
-            ## determine intersections of x + t*u with
-            ## not sure if the following code is necessary
+            ## determine intersections of x + t*u with walls
             if(tmin == -Inf || tmax == Inf){
               stop("problem is unbounded")
             }
+            ## the limits on how far you can go backward and forward
             tmin = max(-c[u>0]); tmax = min(-c[u<0]);
             if(tmin==0 && tmax ==0) {
                 stop("found bad direction")
             }
         }
 
-        ##writeLines(paste("tmin: ", tmin, "\ntmax: ", tmax, "\n", sep = ""))
         ## chose a point on the line segment
         y = y + (tmin + (tmax - tmin)*runif(1))*u;
+        
+        ## choose a point every 'skiplength' samples
         if(i %% skiplength == 0) {
             X[,index] = y
             index = index + 1
