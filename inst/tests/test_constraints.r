79377ee5bf61bf583d288f584ad20d277661bc08
mjfwilliams <mjf2@williams.edu>
2013-07-19 10:48:14 -0400

    added 3d test

--- inst/tests/test_constraints.r
+++ inst/tests/test_constraints.r
@@ -49,6 +49,7 @@ test_that("replace = FALSE works correctly", {
 
 test_that("samples are generated uniformly") {
 
+  ## 2 variables:
   ## A sample of 1000 coordinates for 2 variables should follow binomial
   ## distribution with respect to being above/below .5
   A = matrix(1,ncol = 2)
@@ -57,11 +58,19 @@ test_that("samples are generated uniformly") {
   k = hitandrun(A, b, n = 1000)
   expect_true(all(apply(k, 1, function(x) length(which(x> .5)) < qbinom(.99, 1000, .5))))
 
-
-
-
-
-
-
-
+  ## 3 variables: 
+  A = matrix(1, ncol = 3)
+  b = 1
+  set.seed(1)
+  k = hitandrun(A,b, n = 1000)
+  bins = apply(k, 2, function(x) {
+    ## cool 'hack'
+    bin = (x[1] >= x[3]) + 2*(x[1] >= x[2]) + 4*(x[2] >= x[3])
+    return(bin)
+  })
+  ChiSquared = chisq.test(table(bins))
+  expect_true(ChiSquared$p.value > .01)
+  
+  
 }
+
