cbea8e4188933f85a958c4f165eeff5de711d144
mjfwilliams <mjf2@williams.edu>
2013-07-30 13:16:34 -0400

    added chains

--- R/hitandrun.R
+++ R/hitandrun.R
@@ -38,101 +38,103 @@
 #' b <- c(b,0)
 #' w <- hitandrun(A, b, n = 100) 
 
-hitandrun <- function(A, b = NULL, n, discard = 0, skiplength = 5, verbose = FALSE) {
+hitandrun <- function(A, b, n, discard = 0, skiplength = 5, chains = 1, verbose = FALSE) {
     
     if(n <= 0 || n %% 1 != 0) {
       stop("n must be a positive integer")
     }
-    
-    
-    str = "Finding an intial solution..."
-    if(verbose) cat(str)
-    
-    ## make initial solution = Ap %*% b where 
-    ## Ap is the psuedoinverse of A
-    ## We want to get to solution x of Ax = b
-    ## if A was invertible we could just use
-    ## x = Ai*b, however, A is not square and therefore
-    ## not invertible. Luckily, there exists an object
-    ## called the psuedoinverse Ap such that Ap * A * b = b.
-    ## Ap can be constructed from the SVD of A, where
-    ## SVD(A) = U D V', Ap = V (1/D)' U'. 1/D simply means take
-    ## 1/d for each non-zero entry d, and zero all the others.
-    SVD = svd(A)
-    d = SVD[['d']]
-    V = SVD[['v']]
-    U = SVD[['u']]
-    ## get rid of division errors in d because they will
-    ## mess up 1/d (we keep doing 1/0, so R gives us Inf as well
-    ## we must zero these)
-    d[d < 1e-10] = 0
-    di = 1/d
-    di[di == Inf] = 0
-    if(length(di) <= 1) Dt = matrix(di, ncol = 1, nrow =1) else Dt = t(diag(di))
-    Ap = V %*% Dt %*% t(U)
-    ## l is initial solution
-    l = Ap %*% b
-    
-    ## if l isn't in feasible space use mirror algorithm to find viable solution
-    ## in the interior
-    if(!(all(l > 0))) {
-      if(verbose) for(i in 1:nchar(str)) cat("\b")
-      str = "Using mirror algorithm to find inner solution...\n"
-      if(verbose) cat(str)
-      y = mirror(A, l, 1, verbose)
-    } else {
-      y = l
-    }
-    
-    if(verbose) for(i in 1:nchar(str)) cat("\b")
-
-    ## resolve weird quirk in Null() function
-    if(ncol(A) ==1) {
-        Z = Null(A)
-    } else {
-        Z = Null(t(A))
-    }
-    X = matrix(0, nrow = length(y), ncol = n + discard)
-    if(verbose) cat("Random Walk\nDone with: ")
-    str = "0"
-    if(verbose) cat(str)
+    X = matrix(0, nrow = length(y), ncol = (n + discard)*chains)
     index = 1
-    for(i in 1:(n*skiplength+discard)) {
+    for(chainnum in 1:chains) {
+      str = "Finding an intial solution..."
+      if(verbose) cat(str)
+      
+      ## make initial solution = Ap %*% b where 
+      ## Ap is the psuedoinverse of A
+      ## We want to get to solution x of Ax = b
+      ## if A was invertible we could just use
+      ## x = Ai*b, however, A is not square and therefore
+      ## not invertible. Luckily, there exists an object
+      ## called the psuedoinverse Ap such that Ap * A * b = b.
+      ## Ap can be constructed from the SVD of A, where
+      ## SVD(A) = U D V', Ap = V (1/D)' U'. 1/D simply means take
+      ## 1/d for each non-zero entry d, and zero all the others.
+      SVD = svd(A)
+      d = SVD[['d']]
+      V = SVD[['v']]
+      U = SVD[['u']]
+      ## get rid of division errors in d because they will
+      ## mess up 1/d (we keep doing 1/0, so R gives us Inf as well
+      ## we must zero these)
+      d[d < 1e-10] = 0
+      di = 1/d
+      di[di == Inf] = 0
+      if(length(di) <= 1) Dt = matrix(di, ncol = 1, nrow =1) else Dt = t(diag(di))
+      Ap = V %*% Dt %*% t(U)
+      ## l is initial solution
+      ## http://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_pseudoinverse#Obtaining_all_solutions_of_a_linear_system
+      l = Ap %*% b + (diag(nrow(Ap)) - Ap %*% A) %*% rnorm(nrow(Ap))
+      
+      ## if l isn't in feasible space use mirror algorithm to find viable solution
+      ## in the interior
+      if(!(all(l > 0))) {
+        if(verbose) for(i in 1:nchar(str)) cat("\b")
+        str = "Using mirror algorithm to find inner solution...\n"
+        if(verbose) cat(str)
+        y = mirror(A, l, 1, verbose)
+      } else {
+        y = l
+      }
+      
+      if(verbose) for(i in 1:nchar(str)) cat("\b")
+      
+      ## resolve weird quirk in Null() function
+      if(ncol(A) ==1) {
+        Z = Null(A)
+      } else {
+        Z = Null(t(A))
+      }
+
+      if(verbose) cat("Random Walk\nDone with: ")
+      str = "0"
+      if(verbose) cat(str)
+      for(i in 1:(n*skiplength+discard)) {
         tmin=0;tmax=0;
         while(tmin ==0 && tmax ==0) {
-            ## r is a random unit vector in with basis in Z
-            r = rnorm(ncol(Z))
-            r = r/sqrt(sum(r^2))
-
-            ## u is a unit vector in the appropriate k-plane pointing in a
-            ## random direction Z %*% r is the same as in mirror
-            u = Z%*%r
-            c = y/u
-            ## determine intersections of x + t*u with walls
-            ## the limits on how far you can go backward and forward
-            ## i.e. the maximum and minimum ratio y_i/u_i for negative and positive u.
-            tmin = max(-c[u>0]); tmax = min(-c[u<0]);
-            ## unboundedness
-            if(tmin == -Inf || tmax == Inf){
-              stop("problem is unbounded")
-            }
-            ## if stuck on boundary point
-            if(tmin==0 && tmax ==0) {
-                stop("hitandrun found can't find feasible direction, cannot generate points")
-            }
+          ## r is a random unit vector in with basis in Z
+          r = rnorm(ncol(Z))
+          r = r/sqrt(sum(r^2))
+          
+          ## u is a unit vector in the appropriate k-plane pointing in a
+          ## random direction Z %*% r is the same as in mirror
+          u = Z%*%r
+          c = y/u
+          ## determine intersections of x + t*u with walls
+          ## the limits on how far you can go backward and forward
+          ## i.e. the maximum and minimum ratio y_i/u_i for negative and positive u.
+          tmin = max(-c[u>0]); tmax = min(-c[u<0]);
+          ## unboundedness
+          if(tmin == -Inf || tmax == Inf){
+            stop("problem is unbounded")
+          }
+          ## if stuck on boundary point
+          if(tmin==0 && tmax ==0) {
+            stop("hitandrun found can't find feasible direction, cannot generate points")
+          }
         }
-
+        
         ## chose a point on the line segment
         y = y + (tmin + (tmax - tmin)*runif(1))*u;
         
         ## choose a point every 'skiplength' samples
         if(i %% skiplength == 0) {
-            X[,index] = y
-            index = index + 1
+          X[,index] = y
+          index = index + 1
         }
         if(verbose) for(j in 1:nchar(str)) cat("\b")
         str = paste(i)
         if(verbose) cat(str)
+      }
     }
     if(verbose) cat("\n")
     return(X[,(discard+1):ncol(X)])
